dcf64a9e4bfd1d90a4b5f1bc700d07f89c704940
Ken (Vuken) Nguyen <kenvng@gmail.com>
2022-05-25 15:00:44 -0400

    Update local server and files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Shopping Cart' }); /* render() function tells node or express to send back a response to the browser containing the HTML code of this index file and .hbs */
 });
 
 module.exports = router;
